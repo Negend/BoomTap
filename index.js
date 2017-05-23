0cb1ece02d95b289feafe10be71fa8f6aa27a9a8
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-05-23 14:33:20 +0100

    wires are clickable and respond with their respecive color in console.log

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+$(start)
+var wires = ['red','yellow','pink','blue','orange','purple','white','black','green']
+
+
+
+function start (){
+
+
+
+
+
+
+
+
+	$('.wire').each(function(i,name){
+		$(this).click(function(event){
+			console.log(wires[i])
+		})
+	})
 
 
 
@@ -9,14 +28,4 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-console.log('working')
+}
