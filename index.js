0fc9e3c2a24d94184a19a2d25a1799b02fb6924f
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-05-23 14:55:49 +0100

    color scheme generated randomnly in adjustable array

--- index.js
+++ index.js
@@ -1,22 +1,73 @@
 $(start)
 var wires = ['red','yellow','pink','blue','orange','purple','white','black','green']
-
-
-
+var choice;
+var coloring=[]
+var x;
+var n =3
 function start (){
 
 
+colorChoice()
+colorSequence()
 
 
 
-
-
-
+function colorChoice (){
 	$('.wire').each(function(i,name){
 		$(this).click(function(event){
 			console.log(wires[i])
+			choice = wires[i]
 		})
 	})
+
+}
+
+
+// function colorSequence(){
+
+// 	randomizer(wires.length)
+// 	a = wires[x]
+// 	randomizer(wires.length)
+// 	b = wires[x]
+// 	randomizer(wires.length)
+// 	c = wires[x]
+// 	colorSequence = [a,b,c]
+// }
+function colorSequence(){
+	for(var i = 0; i < n; i++){
+		randomizer(wires.length)
+		coloring.push(wires[x])
+		console.log(coloring)
+	}
+}
+
+
+
+
+
+
+function randomizer(r){
+	x = Math.floor(Math.random()*r)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
